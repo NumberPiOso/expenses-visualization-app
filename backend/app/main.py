bd1847a7a83506e99cc3e5915521ea4233c5847f
Pablo Osorio López <osoriopabl@gmail.com>
2022-01-03 21:33:28 -0500

    Change backend endpoints to /api/

--- backend/app/main.py
+++ backend/app/main.py
@@ -28,18 +28,18 @@ async def root():
     return {"hello": "Main page"}
 
 
-@app.get("/move/{move_id}")
+@app.get("/api/move/{move_id}")
 async def read_item(move_id):
     return {"move_id": move_id}
 
 
-@app.get("/move/", response_model=List[schemas.Move])
+@app.get("/api/move/", response_model=List[schemas.Move])
 def read_moves(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     moves = crud.get_moves(db, skip=skip, limit=limit)
     return moves
 
 
-@app.post("/move/", response_model=schemas.Move)
+@app.post("/api/move/", response_model=schemas.Move)
 async def create_item(move: schemas.MoveCreate, db: Session = Depends(get_db)):
     move_dict = crud.create_move(db, move)
     return move_dict
